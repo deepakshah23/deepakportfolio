c2d3fa4bd64372619ad4e4854935edb615117314
deepakshah23 <138435129+deepakshah23@users.noreply.github.com>
2024-05-25 21:28:40 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
     email: 'deepakshah5348@gmail.com',
     phone: '+919717926128',
     address: 'Gurgaon, Haryana, India - 122002 ',
-    github: 'https://github.com/deepakshah5348',
+    github: 'https://github.com/deepakshah23',
     facebook: 'https://www.facebook.com',
     linkedIn: 'https://www.linkedin.com/in/deepak-shah-638a14236',
     twitter: 'https://twitter.com/deepakshah5348',
